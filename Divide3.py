70d4a35f6b9ed656839451ce4b7784c366ee6d81
Alexander Amos <alexander.amos@beyond-aero.com>
2023-09-18 14:28:54 +0200

    Added cable dummy files

---
+++ Divide3.py
@@ -0,0 +1,9 @@
+def divide3(a):
+
+    print("Hello World!")
+    b = a / 3
+    return b
+
+
+if __name__ == "__main__":
+    rslt = divide3(9)
---
+++ __init__.py
@@ -0,0 +1,2 @@
+from .Divide3 import divide3
+from .Times2 import times2
